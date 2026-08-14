9d4d56780fbace409f38bf656f6e30fb2bfde0b4
Apratim-Raha-CK <apratim.raha@cloudkaptan.com>
2026-08-14 16:16:52 +0530

    Changes made

--- src/app.ts
+++ src/app.ts
@@ -16,7 +16,7 @@ const DUP_DB_FILES= [...DB_FILES]
 
 app.use(express.json())
 app.use(cors({
-  origin: true,
+  origin: 'https://frontend-instance-for-demo-800143476860.asia-south1.run.app',
   credentials: true
 }))
 
