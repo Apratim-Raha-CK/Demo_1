36646d50976743cb9a76fb779acbcbbba80401a0
ApratimRaha <apratim.raha23@gmail.com>
2026-08-15 12:03:42 +0530

    Changes made

---
+++ dist/lib/dbfiles.d.ts
@@ -0,0 +1,10 @@
+declare const DB_FILES: {
+    id: string;
+    file_name: string;
+    file_type: string;
+    file_size: number;
+    created_at: Date;
+    created_by: string;
+}[];
+export default DB_FILES;
+//# sourceMappingURL=dbfiles.d.ts.map
---
+++ dist/lib/users.d.ts
@@ -0,0 +1,9 @@
+declare const USERS: {
+    userid: number;
+    username: string;
+    password: string;
+    role: string;
+    permissions: string[];
+}[];
+export default USERS;
+//# sourceMappingURL=users.d.ts.map
